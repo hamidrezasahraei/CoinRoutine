71c84a1f201ac673ecf03527e3e61744141b378b
Hamidreza <hamidrezasahraei@cafebazaar.ir>
2024-12-28 15:07:04 +0100

    Add Colors and Theme for Light and Dark modes

--- composeApp/src/commonMain/kotlin/dev/coinroutine/app/App.kt
+++ composeApp/src/commonMain/kotlin/dev/coinroutine/app/App.kt
@@ -1,10 +1,13 @@
 package dev.coinroutine.app
 
 import androidx.compose.runtime.Composable
+import dev.coinroutine.app.theme.CoinRoutineTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
+    CoinRoutineTheme {
 
+    }
 }
