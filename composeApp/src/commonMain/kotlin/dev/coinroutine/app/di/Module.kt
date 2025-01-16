9d27f35bba38837eef14c31ab8caa748e652659a
Hamidreza <hamidrezasahraei@cafebazaar.ir>
2025-01-17 00:44:39 +0100

    Provide coins dependencies in sharedModule

--- composeApp/src/commonMain/kotlin/dev/coinroutine/app/di/Module.kt
+++ composeApp/src/commonMain/kotlin/dev/coinroutine/app/di/Module.kt
@@ -1,10 +1,18 @@
 package dev.coinroutine.app.di
 
+import dev.coinroutine.app.coins.data.remote.impl.KtorCoinsRemoteDataSource
+import dev.coinroutine.app.coins.domain.GetCoinDetailsUseCase
+import dev.coinroutine.app.coins.domain.GetCoinsListUseCase
+import dev.coinroutine.app.coins.domain.api.CoinsRemoteDataSource
+import dev.coinroutine.app.coins.presentation.CoinsListViewModel
 import dev.coinroutine.app.core.network.HttpClientFactory
 import io.ktor.client.HttpClient
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.KoinAppDeclaration
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 fun initKoin(config: KoinAppDeclaration? = null) =
@@ -23,4 +31,10 @@ val sharedModule = module {
 
     // core
     single<HttpClient> { HttpClientFactory.create(get()) }
+
+    // coins list
+    viewModel { CoinsListViewModel(get()) }
+    singleOf(::GetCoinsListUseCase)
+    singleOf(::KtorCoinsRemoteDataSource).bind<CoinsRemoteDataSource>()
+    singleOf(::GetCoinDetailsUseCase)
 }
