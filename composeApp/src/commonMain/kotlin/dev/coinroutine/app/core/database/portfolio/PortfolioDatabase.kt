769a0748234893fd4b496191b6c43e67a82f31dc
Hamidreza <hamidrezasahraei@cafebazaar.ir>
2025-01-19 00:35:41 +0100

    Add ConstructedBy to PortfolioDatabase

--- composeApp/src/commonMain/kotlin/dev/coinroutine/app/core/database/portfolio/PortfolioDatabase.kt
+++ composeApp/src/commonMain/kotlin/dev/coinroutine/app/core/database/portfolio/PortfolioDatabase.kt
@@ -1,5 +1,6 @@
 package dev.coinroutine.app.core.database.portfolio
 
+import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import dev.coinroutine.app.portfolio.data.local.PortfolioCoinEntity
@@ -7,6 +8,7 @@ import dev.coinroutine.app.portfolio.data.local.PortfolioDao
 import dev.coinroutine.app.portfolio.data.local.UserBalanceDao
 import dev.coinroutine.app.portfolio.data.local.UserBalanceEntity
 
+@ConstructedBy(PortfolioDatabaseCreator::class)
 @Database(entities = [PortfolioCoinEntity::class, UserBalanceEntity::class], version = 2)
 abstract class PortfolioDatabase: RoomDatabase() {
     abstract fun portfolioDao(): PortfolioDao
