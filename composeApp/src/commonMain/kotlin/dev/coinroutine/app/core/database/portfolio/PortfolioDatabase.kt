5310fb5f34f7b98080731964befa3182a6a334bd
Hamidreza <hamidrezasahraei@cafebazaar.ir>
2025-01-18 14:19:43 +0100

    Add user balance entity and dao

--- composeApp/src/commonMain/kotlin/dev/coinroutine/app/core/database/portfolio/PortfolioDatabase.kt
+++ composeApp/src/commonMain/kotlin/dev/coinroutine/app/core/database/portfolio/PortfolioDatabase.kt
@@ -4,8 +4,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import dev.coinroutine.app.portfolio.data.local.PortfolioCoinEntity
 import dev.coinroutine.app.portfolio.data.local.PortfolioDao
+import dev.coinroutine.app.portfolio.data.local.UserBalanceDao
+import dev.coinroutine.app.portfolio.data.local.UserBalanceEntity
 
-@Database(entities = [PortfolioCoinEntity::class], version = 1)
+@Database(entities = [PortfolioCoinEntity::class, UserBalanceEntity::class], version = 2)
 abstract class PortfolioDatabase: RoomDatabase() {
     abstract fun portfolioDao(): PortfolioDao
+    abstract fun userBalanceDao(): UserBalanceDao
 }
