41b1e4ba90ced6400123f3f7e37cd72ba599e95d
Hamidreza <hamidrezasahraei@cafebazaar.ir>
2025-01-21 01:22:28 +0100

    Setup navigation

--- composeApp/src/commonMain/kotlin/dev/coinroutine/app/App.kt
+++ composeApp/src/commonMain/kotlin/dev/coinroutine/app/App.kt
@@ -1,19 +1,71 @@
 package dev.coinroutine.app
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import dev.coinroutine.app.coins.presentation.CoinsListScreen
+import dev.coinroutine.app.core.navigation.Buy
+import dev.coinroutine.app.core.navigation.Coins
+import dev.coinroutine.app.core.navigation.Portfolio
+import dev.coinroutine.app.core.navigation.Sell
 import dev.coinroutine.app.portfolio.presentation.PortfolioScreen
 import dev.coinroutine.app.theme.CoinRoutineTheme
+import dev.coinroutine.app.trade.presentation.buy.BuyScreen
+import dev.coinroutine.app.trade.presentation.sell.SellScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
+    val navController: NavHostController = rememberNavController()
     CoinRoutineTheme {
-//        CoinsListScreen {  }
-        PortfolioScreen(
-            onCoinItemClicked = {},
-            onDiscoverCoinsClicked = {}
-        )
+        NavHost(
+            navController = navController,
+            startDestination = Portfolio,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            composable<Portfolio> {
+                PortfolioScreen(
+                    onCoinItemClicked = { coinId -> // TODO: will be used later
+                        navController.navigate(Sell)
+                    },
+                    onDiscoverCoinsClicked = {
+                        navController.navigate(Coins)
+                    }
+                )
+            }
+
+            composable<Coins> {
+                CoinsListScreen { coinId -> // TODO: will be used later
+                    navController.navigate(Buy)
+                }
+            }
+
+            composable<Buy> { navBackStackEntry ->
+                BuyScreen(
+                    coinId = "todo",
+                    navigateToPortfolio = {
+                        navController.navigate(Portfolio) {
+                            popUpTo(Portfolio) { inclusive = true }
+                        }
+                    }
+                )
+            }
+            composable<Sell> { navBackStackEntry ->
+                SellScreen(
+                    coinId = "todo",
+                    navigateToPortfolio = {
+                        navController.navigate(Portfolio) {
+                            popUpTo(Portfolio) { inclusive = true }
+                        }
+                    }
+                )
+            }
+
+        }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/dev/coinroutine/app/core/navigation/CoinRoutineScreens.kt
@@ -0,0 +1,15 @@
+package dev.coinroutine.app.core.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Portfolio
+
+@Serializable
+object Coins
+
+@Serializable
+object Buy
+
+@Serializable
+object Sell
