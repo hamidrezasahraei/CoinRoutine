91943d25fdfa572b45c6bb5be0d0d60af80a7797
Hamidreza <hamidrezasahraei@cafebazaar.ir>
2025-01-21 01:32:19 +0100

    Handle safeArgs for coinId in Sell and Buy screens

--- composeApp/src/commonMain/kotlin/dev/coinroutine/app/App.kt
+++ composeApp/src/commonMain/kotlin/dev/coinroutine/app/App.kt
@@ -7,6 +7,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import dev.coinroutine.app.coins.presentation.CoinsListScreen
 import dev.coinroutine.app.core.navigation.Buy
 import dev.coinroutine.app.core.navigation.Coins
@@ -30,8 +31,8 @@ fun App() {
         ) {
             composable<Portfolio> {
                 PortfolioScreen(
-                    onCoinItemClicked = { coinId -> // TODO: will be used later
-                        navController.navigate(Sell)
+                    onCoinItemClicked = { coinId ->
+                        navController.navigate(Sell(coinId))
                     },
                     onDiscoverCoinsClicked = {
                         navController.navigate(Coins)
@@ -40,14 +41,15 @@ fun App() {
             }
 
             composable<Coins> {
-                CoinsListScreen { coinId -> // TODO: will be used later
-                    navController.navigate(Buy)
+                CoinsListScreen { coinId ->
+                    navController.navigate(Buy(coinId))
                 }
             }
 
             composable<Buy> { navBackStackEntry ->
+                val coinId: String = navBackStackEntry.toRoute<Buy>().coinId
                 BuyScreen(
-                    coinId = "todo",
+                    coinId = coinId,
                     navigateToPortfolio = {
                         navController.navigate(Portfolio) {
                             popUpTo(Portfolio) { inclusive = true }
@@ -56,8 +58,9 @@ fun App() {
                 )
             }
             composable<Sell> { navBackStackEntry ->
+                val coinId: String = navBackStackEntry.toRoute<Sell>().coinId
                 SellScreen(
-                    coinId = "todo",
+                    coinId = coinId,
                     navigateToPortfolio = {
                         navController.navigate(Portfolio) {
                             popUpTo(Portfolio) { inclusive = true }
--- composeApp/src/commonMain/kotlin/dev/coinroutine/app/trade/presentation/buy/BuyScreen.kt
+++ composeApp/src/commonMain/kotlin/dev/coinroutine/app/trade/presentation/buy/BuyScreen.kt
@@ -5,17 +5,21 @@ import androidx.compose.runtime.getValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dev.coinroutine.app.trade.presentation.common.TradeScreen
 import dev.coinroutine.app.trade.presentation.common.TradeType
+import io.ktor.http.parametersOf
 import org.koin.compose.viewmodel.koinViewModel
+import org.koin.core.parameter.parametersOf
 
 @Composable
 fun BuyScreen(
     coinId: String,
     navigateToPortfolio: () -> Unit,
 ) {
-    val viewModel = koinViewModel<BuyViewModel>()
+    val viewModel = koinViewModel<BuyViewModel>(
+        parameters = {
+            parametersOf(coinId)
+        }
+    )
     val state by viewModel.state.collectAsStateWithLifecycle()
-
-    //TODO: handle coinId
 
     TradeScreen(
         state = state,
