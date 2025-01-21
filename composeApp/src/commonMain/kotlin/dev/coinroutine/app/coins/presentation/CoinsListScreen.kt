53553426501a5318626a2a5f6053407e2936049d
Hamidreza <hamidrezasahraei@cafebazaar.ir>
2025-01-22 00:39:16 +0100

    Make Android app edge-to-edge

--- composeApp/src/commonMain/kotlin/dev/coinroutine/app/coins/presentation/CoinsListScreen.kt
+++ composeApp/src/commonMain/kotlin/dev/coinroutine/app/coins/presentation/CoinsListScreen.kt
@@ -8,11 +8,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -89,6 +92,7 @@ fun CoinsList(
     ) {
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = WindowInsets.systemBars.asPaddingValues(),
             modifier = Modifier.fillMaxSize(),
         ) {
             item {
